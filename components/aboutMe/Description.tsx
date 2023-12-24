55f4bbbcafd74e4398c37a415df6636b99ffe855
Lucaslive974 <lucasribeirolima974@gmail.com>
2023-12-24 17:59:52 -0300

    Personal description upload

--- components/aboutMe/Description.tsx
+++ components/aboutMe/Description.tsx
@@ -6,7 +6,9 @@ const Description = () => {
     return (
         <S.Description>
                 <h2>About</h2>
-                <main>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</main>
+                <main>
+                    I am a software engineering student, actively engaged in developing personal projects to enhance my skills and gain practical experience. Since childhood, I have harbored a passion for technology and consistently demonstrated a natural ability to grasp its concepts effortlessly. This inherent affinity propels me to constantly seek challenges and refine my knowledge in the field.
+                </main>
                 <div>
                     <ul>
                         <li>
