45d2b0920fcfe98fc6abf53204985c85be94e0f4
lucas.lima <lucasribeirolima974@gmail.com>
2023-12-26 17:06:32 -0300

    Routes Fix

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,5 +5,6 @@ export default (request: VercelRequest, response: VercelResponse) => {
     const { name } = request.body
 
     console.log("Hello ", name);
+
     return response.json({message: 'Hello World'}).status;
 }
